e7e1ea3706fdeb4b54f34847650055c07285e194
Jeremi Andersin <jeremicraft@hotmail.com>
2023-02-01 19:42:07 +0200

    4.6 finished
    In addition, tested with arrays inside an array.

--- 4.1/utils/list_helper.js
+++ 4.1/utils/list_helper.js
@@ -14,11 +14,38 @@ const totalLikes = (likesSum) => {
 const favoriteBlog = (favorites) => {
     //const favorite = favorites.map((lol) => lol.likes)
     //console.log(Math.max(...favorites.map(o => o.likes)))
-    console.log(favorites.find(o => o.likes === Math.max(...favorites.map(o => o.likes))))
+    //console.log(favorites.find(o => o.likes === Math.max(...favorites.map(o => o.likes))))
     return favorites.find(o => o.likes === Math.max(...favorites.map(o => o.likes)))
+}
+const mostBlogs = (most) => {
+    console.log(most.find(m => m.blogs === Math.max(...most.map(m => m.blogs))))
+    const auth = most.find(m => m.blogs === Math.max(...most.map(m => m.blogs)))
+    //const blogs = most.find(m => m.blogs === Math.max(...most.map(m => m.blogs)))
+    console.log(auth.author + " who has " + auth.blogs + " blogs.")
+    return most.find(m => m.blogs === Math.max(...most.map(m => m.blogs)))
+}
+
+//Testing with arrays inside an array.
+const justTesting = (test) => {
+    
+    //const first = test[0][0].blogs
+    const firstNest = test[0].map(m => m.blogs)
+    const firstNestFind = Math.max(...firstNest)
+    const secondNest = test[1][0].blogs
+    console.log(firstNest)
+    console.log(firstNestFind)
+    //console.log(first)
+    if (firstNestFind > secondNest) {
+        return firstNestFind
+    } else {
+        return secondNest
+    }
+    // return test[0].find(m => m.blogs === Math.max(...test[0].map(m => m.blogs)))
 }
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    justTesting
 }
