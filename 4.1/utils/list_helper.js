6fcc30262de5a4e903fe39dcbd67a9159dd77a6a
Jeremi Andersin <jeremicraft@hotmail.com>
2023-02-06 16:23:14 +0200

    4.7 finished

--- 4.1/utils/list_helper.js
+++ 4.1/utils/list_helper.js
@@ -7,45 +7,92 @@ const dummy = (blogs) => {
 const totalLikes = (likesSum) => {
 
     const sum = likesSum.reduce((sum, item) => sum + item.likes, 0)
-    //console.log(sum)
+    
     
     return sum
 }
 const favoriteBlog = (favorites) => {
-    //const favorite = favorites.map((lol) => lol.likes)
-    //console.log(Math.max(...favorites.map(o => o.likes)))
-    //console.log(favorites.find(o => o.likes === Math.max(...favorites.map(o => o.likes))))
+    
     return favorites.find(o => o.likes === Math.max(...favorites.map(o => o.likes)))
 }
 const mostBlogs = (most) => {
-    console.log(most.find(m => m.blogs === Math.max(...most.map(m => m.blogs))))
     const auth = most.find(m => m.blogs === Math.max(...most.map(m => m.blogs)))
-    //const blogs = most.find(m => m.blogs === Math.max(...most.map(m => m.blogs)))
-    console.log(auth.author + " who has " + auth.blogs + " blogs.")
     return most.find(m => m.blogs === Math.max(...most.map(m => m.blogs)))
 }
 
 //Testing with arrays inside an array.
 const justTesting = (test) => {
     
-    //const first = test[0][0].blogs
     const firstNest = test[0].map(m => m.blogs)
     const firstNestFind = Math.max(...firstNest)
     const secondNest = test[1][0].blogs
-    console.log(firstNest)
-    console.log(firstNestFind)
-    //console.log(first)
+
     if (firstNestFind > secondNest) {
         return firstNestFind
     } else {
         return secondNest
     }
-    // return test[0].find(m => m.blogs === Math.max(...test[0].map(m => m.blogs)))
 }
+const mostLikes = (allLikes) => {
+
+    const test = allLikes.filter(m => m.author === 'Robert C. Martin')
+    const tester = test.reduce((sum, item) => sum + item.likes, 0)
+
+    const test2 = allLikes.filter(m => m.author === 'Edsger W. Dijkstra')
+    const tester2 = test2.reduce((sum, item) => sum + item.likes, 0)
+
+    const test3 = allLikes.filter(m => m.author === 'Michael Chan')
+    const tester3 = test3.reduce((sum, item) => sum + item.likes, 0)
+    
+    const arr = [test, test2, test3]
+    const array1 = [tester, tester2, tester3]
+    
+    const arrayResult = array1.find(m => m === Math.max(...array1.map(m => m)))
+
+    if (tester < tester2) {
+        return "Edsger W. Dijkstra"
+    }
+    if (tester < tester3) {
+        return "Michael Chan"
+    }
+    if (tester2 < tester) {
+        return "Robert C. Martin"
+    }
+    if (tester2 < tester3) {
+        return "Michael Chan"
+    }
+    if (tester3 < tester) {
+        return "Robert C. Martin"
+    }
+    if (tester3 < tester2) {
+        return "Edsger W. Dijkstra"
+    }
+    
+}
+const combinedLikes = (allLikes) => {
+    
+    const test = allLikes.filter(m => m.author === 'Robert C. Martin')
+    const tester = test.reduce((sum, item) => sum + item.likes, 0)
+
+    const test2 = allLikes.filter(m => m.author === 'Edsger W. Dijkstra')
+    const tester2 = test2.reduce((sum, item) => sum + item.likes, 0)
+
+    const test3 = allLikes.filter(m => m.author === 'Michael Chan')
+    const tester3 = test3.reduce((sum, item) => sum + item.likes, 0)
+    
+    const array1 = [tester, tester2, tester3]
+    const arrayResult = array1.find(m => m === Math.max(...array1.map(m => m)))
+
+    return arrayResult
+}
+
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
     mostBlogs,
-    justTesting
+    justTesting,
+    mostLikes,
+    combinedLikes
 }
